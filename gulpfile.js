321bbfbed0f19e55ec60fb96db1d1474c804c243
Wujek <mjwojcik94@gmail.com>
2017-02-25 10:48:43 +0100

    error fix

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,21 @@ gulp.task('siema',function() { //nazwa taska
 
 //KOMPILACJA SCSS >> CSS
 gulp.task('sass',function(){ //kompilator SASS
+   
    return gulp.src('app/scss/**/*.scss') //przeszukuje wszystkie w scss
    .pipe(sass())
+   .on('error', function (err) {
+            console.log(err.toString());
+
+            this.emit('end');
+        })
    .pipe(gulp.dest('app/css'));
+   
 });
 
 //OBSERWOWANIE ZMIAN W PLIKACH
 gulp.task('watch',function(){ //
+   
 // obserwuj  >>CO i gdzie       >> TABLICA TASKÓW
  gulp.watch('app/scss/**/*.scss', ['sass']);
    
